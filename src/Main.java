e87b7cfe4eb63c98ed6f4459b3282b8a5cc84f87
mz0113 <1172295848@qq.com>
2019-05-31 20:42:24 +0800

    9

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,20 @@ import java.util.concurrent.ForkJoinTask;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        SearchTask searchTask = new SearchTask(4,new Data(),0,Data.longs.length);
-        ForkJoinPool forkJoinPool = new ForkJoinPool(6);
-        ForkJoinTask forkJoinTask = forkJoinPool.submit(searchTask);
-        Scanner scanner = new Scanner(System.in);
-        scanner.next();
-/*        try {
-            //System.out.println(forkJoinTask.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }*/
-  //      ffffff9890
+	public static void main(String[] args) {
+		SearchTask searchTask = new SearchTask(4, new Data(), 0, Data.longs.length);
+		ForkJoinPool forkJoinPool = new ForkJoinPool(6);
+		ForkJoinTask forkJoinTask = forkJoinPool.submit(searchTask);
+		Scanner scanner = new Scanner(System.in);
+		scanner.next();
+		/*
+		 * try { //System.out.println(forkJoinTask.get()); } catch (InterruptedException
+		 * e) { e.printStackTrace(); } catch (ExecutionException e) {
+		 * e.printStackTrace(); }
+		 */
+		// ffffff9890
 //87678
-        //342432
-        //999
-    }
+		// 342432
+		// 999
+	}
 }
